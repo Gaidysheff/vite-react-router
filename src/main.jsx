38db6c0f8644162deb84142035894269f33bdbff
Gaidysheff <gaidysheff@mail.ru>
2025-04-14 15:05:18 +0500

    third

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,11 @@ import "./index.css";
 
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
